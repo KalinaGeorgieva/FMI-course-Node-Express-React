bee81f4f47a35208928d2205b04ced146c5208da
Kalina Georgieva <kallygeorgieva@gmail.com>
2017-05-08 17:45:51 +0300

    Create add component for adding post

--- Simple blogging/app/components/posts/add.js
+++ Simple blogging/app/components/posts/add.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
-const Add = ()=> {
+const Add = () => {
     return (
-        <div id="app-page">Add</div>
+        <div id="app-page" className="add-new-post-container">
+            <div className="panel panel-info col-lg-7 text-center">
+                <div className="panel-body">
+                    <div id="author-post" className="clearfix">
+                            <span>Author: </span>
+                            <input type="text" id="author-post-value" placeholder="Author"/>
+                    </div>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <div id="title-post">
+                            <span>Title: </span>
+                            <input type="text" id="title-post-value" placeholder="Post title"/>
+                    </div>
+                    <textarea placeholder="Write your post here!" className="add-new-post-textarea"></textarea>
+                        
+                    
+                        <div className="btn-group btn-icons">
+                            <button className="btn btn-md" id="btn-post-text" type="button"><span className="glyphicon glyphicon-text-background"></span></button>
+                            <button className="btn btn-md" id="btn-post-picture" type="button"><span className="glyphicon glyphicon-picture"></span></button>
+                        </div>
+                        <button className="btn btn-primary pull-right btn-add-new-post" type="button">Post</button>
+                    
+                </div>
+            </div>
+        </div>
     )
 }
 
