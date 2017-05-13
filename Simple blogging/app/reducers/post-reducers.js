c54382205572c00f9d655143ea42acd893d323ec
Kalina Georgieva <kallygeorgieva@gmail.com>
2017-05-13 12:20:00 +0300

    Fix editPost reducer

--- Simple blogging/app/reducers/post-reducers.js
+++ Simple blogging/app/reducers/post-reducers.js
@@ -1,26 +1,54 @@
 import {ADD_POST, REMOVE_POST, EDIT_POST, CHANGE_STATUS} from '../config/constants'
 import { combineReducers } from "redux"
+import merge from 'lodash/merge'
 
 
 const initialState = {
   posts: [
-      { id: 0,
-        title: "Initial Post",
-        author: "Kalina",
-        msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        tags: "#love #sun #happy",
-        status: true,
-        url: "http://cdn.images.express.co.uk/img/dynamic/1/590x/dog-650299.jpg",
-        timestamp: Date.now()
-    }, { id: 1,
-        title: "Second Post",
-        author: "Kalina",
-        msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        tags: "#love #sun #happy",
-        status: false,
-        url: "https://www.pets4homes.co.uk/images/breeds/55/large/2525bdb43f08d9b6861b1d089f96fef2.jpg",
-        timestamp: Date.now()
-    }
+    //   { id: 0,
+    //     title: "Initial Post",
+    //     author: "Kalina",
+    //     msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    //     tags: "#love #sun #happy",
+    //     status: true,
+    //     url: "http://cdn.images.express.co.uk/img/dynamic/1/590x/dog-650299.jpg",
+    //     timestamp: Date.now()
+    // }, { id: 1,
+    //     title: "1",
+    //     author: "Kalina",
+    //     msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    //     tags: "#love #sun #happy",
+    //     status: false,
+    //     url: "https://www.pets4homes.co.uk/images/breeds/55/large/2525bdb43f08d9b6861b1d089f96fef2.jpg",
+    //     timestamp: Date.now()
+    // },
+    // { id: 2,
+    //     title: "2",
+    //     author: "Kalina",
+    //     msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    //     tags: "#love #sun #happy",
+    //     status: false,
+    //     url: "https://www.pets4homes.co.uk/images/breeds/55/large/2525bdb43f08d9b6861b1d089f96fef2.jpg",
+    //     timestamp: Date.now()
+    // },
+    // { id: 3,
+    //     title: "3",
+    //     author: "Kalina",
+    //     msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    //     tags: "#love #sun #happy",
+    //     status: false,
+    //     url: "https://www.pets4homes.co.uk/images/breeds/55/large/2525bdb43f08d9b6861b1d089f96fef2.jpg",
+    //     timestamp: Date.now()
+    // },
+    // { id: 4,
+    //     title: "4",
+    //     author: "Kalina",
+    //     msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    //     tags: "#love #sun #happy",
+    //     status: false,
+    //     url: "https://www.pets4homes.co.uk/images/breeds/55/large/2525bdb43f08d9b6861b1d089f96fef2.jpg",
+    //     timestamp: Date.now()
+    // }
   ],
   status: "ALL"
 }
@@ -55,12 +83,14 @@ function postReducer(state = initialState, action) {
         case CHANGE_STATUS:
             return {status: action.status, posts: state.posts};
         case EDIT_POST:
-            let newPosts;
-            if (state.posts.length === 1) {
-                newPosts = [JSON.parse(JSON.stringify(action.post))]
-            } else{
-                newPosts = [ ...state.posts.slice(0, action.post.id), action.post, ...state.posts.slice(action.post.id + 1)]
-            }
+            let newPosts = state.posts.map((post)=> {
+                debugger;
+                if(action.post.id !== post.id) {
+                    return post
+                }
+                return action.post;
+            })
+
             return {
                 posts: newPosts,
                 status: state.status
